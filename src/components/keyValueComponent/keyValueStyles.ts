9dabc525d7e9bb58e2409d7b1d40c94d64f06565
Robert <cardmechanic@gmx.de>
2021-12-08 00:49:13 +0100

    small adjustment

--- src/components/keyValueComponent/keyValueStyles.ts
+++ src/components/keyValueComponent/keyValueStyles.ts
@@ -8,13 +8,16 @@ export const KeyField = styled.div`
     font-size: 18px;
     margin: 16px;
     width: 50%;
-    color: ${theme.primaryColor}
+    color: ${theme.primaryColor};
+    @media only screen and (max-width: 540px) {
+        margin: 4px;
+     }
 `;
 
 export const ValueField = styled(KeyField)`
     font-weight: 400;
     color: ${theme.secondaryColor};
-    @media only screen and (max-width: 540px) {
+    @media only screen and (max-width: 810px) {
        text-align: left;
     }
 `;
